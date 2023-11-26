8c1dda6a4af8190c995442082b5beb1309cbca54
programmerrabbanii <rabbanisarkar642@gmail.com>
2023-11-26 11:34:20 +0600

    added the navar section here

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,10 +1,39 @@
+import { Link } from "react-router-dom";
+import SocailLogin from "../../Components/SocailLogin/SocailLogin";
 
 const Login = () => {
+    const handleLogin=e=>{
+        e.preventDefault()
+        const form=e.target;
+        const name=form.name.value;
+        const email=form.email.value;
+        const password=form.password.value;
+        const allLoginuser={name,email,password}
+        console.log(allLoginuser)
+    }
+
     return (
         <div>
-            <h4>this is login</h4>
+           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-14 items-center px-14">
+            <div>
+                <img src="https://i.ibb.co/BjFsCxg/3094352.jpg" alt="" />
+            </div>
+            <div>
+                <h2 className="text-2xl py-5">Pleass Login </h2>
+                <form onSubmit={handleLogin}>
+                    <input className="w-full border py-2 px-2 mb-2"  type="text" name="name" placeholder="Type Your NAme" /> <br />
+                    <input  className="w-full border py-2 px-2 mb-2" type="email" name="email" placeholder="Type Your Name" /> <br />
+                    <input  className="w-full border py-2 px-2 mb-2" type="password" name="password" placeholder="Type Your Password" /> <br />
+                    <input className=" cursor-pointer w-full border py-2 px-2 mb-2"  type="submit" value="SUBMIT"/>
+                </form>
+                <SocailLogin></SocailLogin>
+                <Link to='/register'>
+                <p>Not registered? <button className="uppercase"> Create account</button></p>
+                </Link>
+            </div>
+           </div>
             
-        </div>
+        </div> 
     );
 };
 
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -2,17 +2,30 @@ import { Link } from "react-router-dom";
 import SocailLogin from "../../Components/SocailLogin/SocailLogin";
 
 const Register = () => {
+    const handleRegister= e=>{
+        e.preventDefault()
+        const form=e.target;
+        const name=form.name.value;
+        const email=form.email.value;
+        const password=form.password.value;
+        const photoURL=form.photoURL.value;
+        const  allRegisteruser={name,email,password,photoURL}
+        console.log(allRegisteruser)
+        
+    }
+        
+    
     return (
        <div className="registerBg px-14 mt-36">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-10 ">
             <div>
                 <h2 className="text-2xl capitalize py-5">Registe Now</h2>
-                <form>
+                <form onSubmit={handleRegister}>
                     <input className="w-full py-2 px-2 border mb-2" type="text" name="name" placeholder="Type Your Name" /> <br />
                     <input className="w-full py-2 px-2 border mb-2" type="email" name="email" placeholder="Type Your Email"/> <br />
                     <input className="w-full py-2 px-2 border mb-2" type="password" placeholder="Type Your Password"name="password" /> <br />
-                    <input className="w-full py-2 px-2 border mb-2" type="text" name="photURL" placeholder="PhotoURL" /><br /> 
-                    <input className="w-full py-2 px-2 border mb-2" type="submit" value="sumbit" />
+                    <input className="w-full py-2 px-2 border mb-2" type="text" name="photoURL" placeholder="PhotoURL" /><br /> 
+                    <input className=" uppercase cursor-pointer w-full py-2 px-2 border mb-2" type="submit" value="sumbit" />
                 </form>
                 <SocailLogin></SocailLogin>
                 <Link to='/login'>
@@ -22,7 +35,7 @@ const Register = () => {
 
             </div>
             <div>
-                <img className="h-screen w-full bg-cover" src="https://i.ibb.co/LC1JgsS/images-2.png" alt="" />
+                <img className="h-screen w-full bg-cover" src="https://i.ibb.co/BjFsCxg/3094352.jpg" alt="" />
             </div>
         </div>
 
